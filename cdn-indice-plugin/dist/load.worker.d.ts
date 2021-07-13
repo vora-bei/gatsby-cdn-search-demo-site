6677874e6638eb1ca4ce9e1c623d84e7e15bf207
Nikita Vorobyev <voronsupermen@gmail.com>
2021-07-13 22:43:36 +0300

    fix speed

---
+++ cdn-indice-plugin/dist/load.worker.d.ts
@@ -0,0 +1,3 @@
+export { restoreDb };
+import { restoreDb } from "browser";
+//# sourceMappingURL=load.worker.d.ts.map
---
+++ cdn-indice-plugin/dist/load.worker.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.restoreDb = void 0;
+var browser_1 = require("browser");
+Object.defineProperty(exports, "restoreDb", { enumerable: true, get: function () { return browser_1.restoreDb; } });
+//# sourceMappingURL=load.worker.js.map
