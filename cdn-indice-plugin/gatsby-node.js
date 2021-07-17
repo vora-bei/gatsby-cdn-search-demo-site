1f70ba601128b30ea817bdf462053499f53d0878
Nikita Vorobyev <voronsupermen@gmail.com>
2021-07-17 13:11:02 +0300

    remove worker plugin

--- cdn-indice-plugin/gatsby-node.js
+++ cdn-indice-plugin/gatsby-node.js
@@ -48,15 +48,3 @@ exports.pluginOptionsSchema = ({ Joi }) => {
     }).or('column', 'columns')),
   })
 }
-exports.onCreateWebpackConfig = ({ actions: { replaceWebpackConfig }, getConfig }) => {
-  const config = getConfig()
-
-  config.module.rules.push({
-    test: /\.worker\.js$/,
-    use: { loader: 'workerize-loader' }
-  })
-
-  config.output.globalObject = 'this'
-
-  replaceWebpackConfig(config)
-}
